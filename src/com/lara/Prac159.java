4f944f57571d7dab9f1b06190ef7547f6baad90f
Avanish <palavanish920@gmail.com>
2025-07-17 14:31:43 +0530

    This is second commit

---
+++ src/com/lara/Prac159.java
@@ -0,0 +1,9 @@
+package com.lara;
+
+public class Prac159 {
+    public static void main(String[] args) {
+      StringBuffer sb= new StringBuffer("Avanish");
+      sb.append(" Pal");
+        System.out.println(sb);
+    }
+}
